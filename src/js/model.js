e99b5c630669e9f0e6590e357fce41376f4e8088
Irena Koval <i.irenakoval@gmail.com>
2024-07-03 20:36:52 +0300

    add config and helpers

--- src/js/model.js
+++ src/js/model.js
@@ -1,4 +1,6 @@
 import { async } from "regenerator-runtime";
+import { API_URL } from "./config";
+import { getJSON } from "./helpers";
 
 export const state = {
 	recipe: {},
@@ -6,11 +8,8 @@ export const state = {
 
 export const loadRecipe = async function (id) {
 	try {
-        const res = await fetch(`https://forkify-api.herokuapp.com/api/v2/recipes/${id}`);
-        const data = await res.json();
+        const data = await getJSON(`${API_URL + id}`)
         console.log(data);
-
-        if (!res.ok) throw new Error(`${data.message} (${res.status})`);
         const { recipe } = data.data;
         state.recipe = {
             id: recipe.id,
@@ -24,6 +23,6 @@ export const loadRecipe = async function (id) {
         };
         console.log(state.recipe)
     } catch (err) {
-        alert(err)
+        console.error(err)
     }
 };
